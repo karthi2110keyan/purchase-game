5253212f7c94f20ba5752d4a851c287635f0c895
Karthi <155168516+karthi2110keyan@users.noreply.github.com>
2024-06-03 09:41:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+const slider = document.getElementById("animate")
+let container =document.querySelector(".slide-container")
+let index=0
+let pixel=0
+
+
+var intervel = setInterval(slideranime,3000)
+
+
+slider.addEventListener("mouseover",function(){
+
+  clearInterval(intervel,100)
+    
+    console.log("helolo")
+
+})
+
+
+
+
+
+
+
+function slideranime(){
+
+    if(index===0){
+        
+       slider.style.transform=`translate(${pixel}px)`
+    }else {
+        pixel-=350
+        slider.style.transform=`translate(${pixel}px)`
+    }
+    index++
+    if(index===8){
+       
+        index=0
+        pixel=0
+    }
+    
+} 
+
+
+/* nav section */
+
+var nav = document.querySelector(".sec-four")
+var openbtn=document.querySelector(".nav-icon")
+var closebtn =document.querySelector(".close-button")
+
+openbtn.addEventListener("click",function(){
+    nav.style.transform="translateX(0px)"
+})
+
+closebtn.addEventListener("click",function(){
+    nav.style.transform="translateX(-420px)"
+})
+
+
+
+import { gamevideo } from "./script/product.js"
+var container_box=document.querySelector(".inner-container")
+ let html;
+gamevideo.forEach((gameFile)=>{
+
+    html=`
+                <a href="" class="box">
+                    <img class="games-image" src="${gameFile.image}" width="100" height="100">
+
+                        <div class="name">
+                         <p>${gameFile.name}</p>
+                         <p style="color:green; text-align:end;">${gameFile.rate}</p>
+                        </div>
+                </a>
+               
+            `
+
+            container_box.innerHTML+=html
+}) 
---
+++ script/product.js
@@ -0,0 +1,18 @@
+export const gamevideo =[{
+    image:"./image/god.webp",
+    name:"GOD OF WAR 4",
+    rate:"$2.99"
+
+},{
+    image:"./image/gta.webp",
+    name:"GOD OF WAR 4",
+    rate:"$2.99"  
+},{
+    image:"./image/red.jpg",
+    name:"GOD OF WAR 4",
+    rate:"$2.99"  
+},{
+    image:"./image/witcher.jpg",
+    name:"GOD OF WAR 4",
+    rate:"$2.99"  
+}]
